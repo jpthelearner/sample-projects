dc8a68d627eb45384df3985c859d2dfacb257162
jpthelearner <kjeyaprakash@gmail.com>
2013-09-07 22:56:47 -0700

    gradle java sample project

---
+++ gradle/jar_proj_tmpl/src/main/java/com/jp/sample/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.jp.sample;
+
+public class HelloWorld {
+	private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(HelloWorld.class);
+
+	public String sayHello() {
+		return null;	
+	}
+}
---
+++ gradle/jar_proj_tmpl/src/test/java/com/jp/sample/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.jp.sample;
+
+import org.junit.Test;
+
+import com.jp.sample.HelloWorld;
+
+public class HelloWorldTest {
+
+        @Test
+        public void sayHelloTest() {
+		org.junit.Assert.fail("Not yet implemented");
+        }
+
+}
+
